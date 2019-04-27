aaa05bd3dc6cbcb54d40ab19ead16473750b464c
Tyler Anton <mtyleranton@gmail.com>
2019-04-27 15:19:20 -0700

    update player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,22 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotateSpeed = 300.0f;
+    public float gravity = 20.0f;
 
-    private float translation;
-    private float rotation;
+    private CharacterController _controller;
+    private float _rotation;
+    private Vector3 _moveDirection = Vector3.zero;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+    {   
+        _rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        transform.Rotate(0, _rotation, 0);
+        
+        _moveDirection = Vector3.forward * Input.GetAxis("Vertical");
+        _moveDirection = transform.TransformDirection(_moveDirection);
+        _moveDirection *= speed;
+
+        _moveDirection.y -= gravity * Time.deltaTime;
+
+        _controller.Move(_moveDirection * Time.deltaTime);
     }
 }
