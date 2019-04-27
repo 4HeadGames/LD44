e0c2fa05c4973c8a7ee4e6bdaef73fa54252eae4
Tyler Anton <mtyleranton@gmail.com>
2019-04-27 00:24:22 -0700

    Base ability system

---
+++ Assets/Scripts/Abilities/AbilityHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityHandler : MonoBehaviour
+{
+    public GameObject ability1;
+    public GameObject ability2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ability1.GetComponent<IAbility>().Activate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ability2.GetComponent<IAbility>().Activate();
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/Ghost.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour, IAbility
+{
+    public void Activate()
+    {
+        Debug.Log("Ghost activated.");
+    }
+}
