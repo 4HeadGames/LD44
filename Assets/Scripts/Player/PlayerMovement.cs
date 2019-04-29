2cc9379aad63bfbf10f90dc538fb3e9dee317d5b
Tyler Anton <mtyleranton@gmail.com>
2019-04-28 18:04:35 -0700

    player movement animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,11 +9,13 @@ public class PlayerMovement : MonoBehaviour {
     public float dashSpeed = 50.0f;
     public float gravity = 20.0f;
 
+    private Animator _animator;
     private CharacterController _controller;
     private float _rotation;
     private Vector3 _moveDirection = Vector3.zero;
 
     void Start() {
+        _animator = GetComponent<Animator>();
         _controller = GetComponent<CharacterController>();
     }
 
@@ -26,11 +28,18 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Move() {
-        _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        transform.forward = _moveDirection;
-        _moveDirection *= speed;
-
-        _controller.SimpleMove(_moveDirection);
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+            _animator.SetBool("isRunning", true);
+            _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            transform.forward = _moveDirection;
+            _moveDirection *= speed;
+            _controller.SimpleMove(_moveDirection);
+        }
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            _animator.SetBool("isRunning", false);
+        }
     }
 
     void Dash() {
